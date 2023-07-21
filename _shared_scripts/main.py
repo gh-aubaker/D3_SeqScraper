9e634aca3ef5845de920afe0591f01ed0af4877d
Aryan Patel <aryan.college155@gmail.com>
2023-07-21 08:36:08 -0400

    cleaned up code and files

--- _shared_scripts/main.py
+++
@@ -1,7 +0,0 @@
-import time
-import JSONscrub
-
-if __name__ == '__main__':
-    start = time.time()
-    JSONscrub.execute()
-    print("done. it took ", time.time()-start, " to run")
--- _shared_scripts/paths.py
+++
@@ -1,8 +0,0 @@
-def outputPath():
-    return "_outputfiles/readings_summary.csv"
-def inputPath():
-    return "_inputfiles/D3_ProgPerPCM.json"
-def keyVal():
-    return "PCM Number"
-def valueVal():
-    return "Program Name"
